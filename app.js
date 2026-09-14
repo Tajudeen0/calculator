aa89320aee9eb4745ecb05ff78594027e740765e
Adebanjo Timothy <adebanjotimothy3235@gmail.com>
2026-09-14 19:14:00 +0100

    populate special buttons functions and edge cases

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function add(firstValue, secondValue) {
-    return (firstValue+secondValue);
+
+    return (parseInt(firstValue)+parseInt(secondValue));
 };
 function subtract(firstValue, secondValue) {
     return (firstValue-secondValue);
@@ -8,57 +9,113 @@ function multiply(firstValue, secondValue) {
     return (firstValue*secondValue);
 };
 function divide(firstValue, secondValue) {
-    return (firstValue/secondValue);
+    if (!(secondValue == 0)) {
+         return (firstValue/secondValue);
+    } else {
+        return "math Error"
+    }
+   
 };
-//0 = add, 1 = subtract, 2 = multiply, 3 =  divide.
-function operate(operatorUsed, firstValue, secondValue) {
-    if (operatorUsed == 0) {
+
+function operate(operatorUsed , firstValue, secondValue) {
+    if (operatorUsed == "+") {
         return add(firstValue, secondValue)
-    } else if (operatorUsed == 1) {
+    } else if (operatorUsed == "-") {
         return subtract(firstValue, secondValue)
-    } else if (operatorUsed == 2) {
+    } else if (operatorUsed == "x") {
         return multiply(firstValue, secondValue)
-    } else if (operatorUsed == 3) {
+    } else if (operatorUsed == "/") {
         return divide(firstValue, secondValue)
+    } else if (operator == "" ) {
+        return firstValue;
     }
 } ;
 
 let initialNumber = "";
-let subsequentNumber = 0;
+let subsequentNumber = "";
 let operator = "";
 
 const numberButton = document.querySelectorAll(".nb");
 const operatorButton = document.querySelectorAll(".ftn-btn");
-
-console.table(operatorButton)
-
-
-
+const specialButton = document.querySelectorAll(".op-btn");
 
 
 numberButton.forEach(button => {
     button.addEventListener('click', () => {
-        btnValue = button.textContent;
-        initialNumber = btnValue;
         console.log(initialNumber)
+        btnValue = button.textContent;
         const input = document.querySelector(".input-text");
-        input.textContent +=  initialNumber;
+        if (operator == "") {
+            initialNumber += btnValue;
+            input.textContent +=  btnValue;
+        }   else {
+            subsequentNumber += btnValue;
+            input.textContent += btnValue;
+        }
+        console.log(initialNumber, operator, subsequentNumber,);
     })
 })
 
 operatorButton.forEach(button => {
     button.addEventListener('click', () => {
+        const result = document.querySelector(".result-text")
+        const input = document.querySelector(".input-text")
         btnValue = button.textContent;
-        operator = btnValue;
-        console.log(initialNumber)
-        const input = document.querySelector(".input-text");
-        input.textContent +=  operator;
+        if (operator == "") {
+            operator = btnValue;
+            input.textContent +=  operator;
+
+        }  else if (!(operator == "") && (subsequentNumber == "")) {
+            input.textContent =  input.textContent.slice(0,-1);
+            input.textContent += btnValue;   
+            operator = btnValue;   
+        } else if (!(operator == "") && !(subsequentNumber == "")) {
+            result.textContent = operate(operator, initialNumber, subsequentNumber)
+            initialNumber = result.textContent;
+            input.textContent = result.textContent;
+            subsequentNumber = "";
+            operator = btnValue;
+            console.log(initialNumber);
+            input.textContent +=  operator;
+        }
+            
+            
     })
 })
 
+specialButton.forEach(button => 
+    button.addEventListener('click', () => {
+        btnValue =  button.textContent;
+        input = document.querySelector(".input-text");
+        result = document.querySelector(".result-text");
+        if (btnValue == "CLR") {
+            initialNumber = "";
+            subsequentNumber = "";
+            operator = "";
+            input.textContent = "";
+            result.textContent = "";
+        } else if (btnValue == "DEL") {
+            //populate ths later
+            if ((!(subsequentNumber) == "" ) && !(operator == "")){
+                console.log(initialNumber)
+                subsequentNumber = "";
+                input.textContent =  input.textContent.slice(0,-1);
+                console.log(initialNumber)
+              } else if (!(operator == "") && (subsequentNumber == "")) {
+                operator = "";
+                input.textContent = input.textContent.slice(0,-1);
+            }
+            }  
 
-
-
+         else if (btnValue == "=") {
+            
+            result.textContent = operate(operator, initialNumber, subsequentNumber);
+            initialNumber = result.textContent;
+            subsequentNumber = "";
+         }
+    })
+)
+    
 
 
 
